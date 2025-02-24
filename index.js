4da40fa1f53c23b40d1cace3b56309dfd878d252
ManuMelva <emmanuelsilva101@outlook.com>
2025-02-24 19:37:07 -0300

    fix: adiciona validação para caso o nome não seja informado

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // index.js
 
 function greet(name) {
+    if (!name) {
+      return "Olá! Por favor, informe um nome.";
+    }
     return `Olá, ${name}! Seja bem-vindo(a) ao nosso software.`;
   }
   
-  console.log("Versão 1.1");
-  console.log(greet("Alice"));
+  console.log("Versão 1.2");
+  console.log(greet(""));
   
