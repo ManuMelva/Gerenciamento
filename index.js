b877558bee434197e2625408d45e5af90a68fdeb
ManuMelva <emmanuelsilva101@outlook.com>
2025-02-24 19:36:53 -0300

    feat: adiciona função de saudação personalizada

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Versão 1.0 - Olá, mundo!");
+// index.js
+
+function greet(name) {
+    return `Olá, ${name}! Seja bem-vindo(a) ao nosso software.`;
+  }
+  
+  console.log("Versão 1.1");
+  console.log(greet("Alice"));
+  
